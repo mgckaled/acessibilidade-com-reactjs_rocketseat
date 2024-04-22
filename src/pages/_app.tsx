a6e0c04f14a15960997e82d280ddd25f20922e59
mgckaled <marcelgckaled@gmail.com>
2024-04-22 16:35:27 -0300

    start next.js project and set first site configurations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from "next/app"
+import { Roboto } from "next/font/google"
+
+import "@/styles/globals.css"
+
+const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"], display: "swap" })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />;
+    </main>
+  )
 }
